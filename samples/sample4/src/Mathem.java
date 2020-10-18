f6ee28f2dbffbaac190c43737f7b87fe07591c39
wai-C <73032390+wai-C@users.noreply.github.com>
2020-10-18 13:05:19 +0300

    The implementation
    One using Recursion,One using dynamic

--- samples/sample4/src/Mathem.java
+++ samples/sample4/src/Mathem.java
@@ -12,10 +12,25 @@ public class Mathem {
 // Please implement this fibonnachi method using recursion
 //
 //	// 1 1 2 3 5 8 13 21 .... (Nx = N(x-1) + N(x-2))
-//	int [] fib(int len) {
-//		int arr[] = new int [len];
-//		
-//	}
-//	
-//	fibNext()
+//using recursion
+	public int fibRe(int n) {
+		if(n==0) {
+		return 0;
+		}
+		if(n==1||n==2) {
+			return 1;
+		}
+		else 
+			return fibRe(n-2)+fibRe(n-1);
+	}
+	//using dynamic
+	public int fibDy(int n) {
+		int fDy[]= new int[n+1];
+		fDy[0]=0; //[0] index in Fib.Sequence is 0
+		fDy[1]=1; //[1] index in Fib.Sequence is 1
+		for(int i=2;i<=n;i++) {
+			fDy[i]=fDy[i-2]+fDy[i-1];
+		}
+		return fDy[n];	
+	}
 }
